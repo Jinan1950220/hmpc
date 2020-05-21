c919cb8439e98f87758af774f810ab6051dae2f9
Jinan19950220 <920253667@qq.com>
2020-05-21 14:24:39 +0800

    完成初始化更改

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// 脚手架自动创建的路由配置文件
+// 由于目录下有一个名为index.js的文件，则可以简写：
+// import XXX from './router/index.js' ======>
+// import XXX from './router'
 import router from './router'
+
+import './style/index.less'
 
 Vue.config.productionTip = false
 
